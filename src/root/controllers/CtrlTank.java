cc0aa5b3279e4ecac5e3ffeb015530817d4c93c1
axel <polozhencev16@bk.ru>
2020-04-02 13:43:59 +0300

    убрал лишние комментарии

--- src/root/controllers/CtrlTank.java
+++ src/root/controllers/CtrlTank.java
@@ -12,16 +12,6 @@ public class CtrlTank extends ControlObject<Tank> {
         this.mainTank = mainTank;
         addObject(mainTank);
         setViewer(new VTank());
-
-//        loader = new Loader<>("tanks_v1.map");
-//
-//        try {
-//            addObject(loader.loadFromFile());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
@@ -30,10 +20,6 @@ public class CtrlTank extends ControlObject<Tank> {
         for (int i = 0; i < tanks.size(); i++) {
             Tank tank = tanks.get(i);
             tank.update();
-
-//            if(tank.getState() == 0){
-//                removeObject(tank);
-//            }
         }
     }
 
